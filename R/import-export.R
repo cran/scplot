d104e023ee7207988501b92612b635ddb8dbe0de
Juergen Wilbert <juergen.wilbert@uni-muenster.de>
2026-02-25 07:30:02 +0000

    version 0.6.1

--- R/import-export.R
+++ R/import-export.R
@@ -2,9 +2,6 @@
 ggplot2::unit
 
 #' @export
-scan::`%>%`
-
-#' @export
 ggplot2::element_line
 
 #' @export
@@ -13,4 +10,6 @@ ggplot2::element_text
 #' @export
 ggplot2::element_rect
 
+#' @export
+ggplot2::element_point
 
